de7a742f37c8f0d8f346178f836e64511c203aee
LarsZauberer <wasser.ian@gmail.com>
2025-08-26 17:01:52 +0200

    Basic Bevy setup

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    fenix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "devShell";
+
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+
+          pkg-config
+          udev
+          alsa-lib
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          libxkbcommon
+          wayland
+          glibc.dev
+          glib.dev
+        ];
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.vulkan-loader
+          pkgs.libxkbcommon
+          pkgs.wayland
+          pkgs.alsa-lib
+          pkgs.udev
+          pkgs.xorg.libX11
+          pkgs.xorg.libXcursor
+          pkgs.xorg.libXi
+          pkgs.xorg.libXrandr
+        ];
+      };
+    });
+}
