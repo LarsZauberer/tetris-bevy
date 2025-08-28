8f160be47b81377ae59cc13659fbf955bfe38485
LarsZauberer <wasser.ian@gmail.com>
2025-08-28 16:07:25 +0200

    Cleanup and adding some docstrings

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use bevy::prelude::*;
 const UNIT: f32 = 50.0;
 const WIDTH: f32 = 10.0;
 const HEIGHT: f32 = 20.0;
-const EMPTY_COLOR: i32 = 0x2b2b2b;
 
 fn main() {
     let mut app = App::new();
@@ -19,9 +18,8 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut window: Single<&mut Window>,
-    world: ResMut<World>,
 ) {
-    window.resolution.set(UNIT * 10.0, UNIT * 20.0);
+    window.resolution.set(UNIT * WIDTH, UNIT * HEIGHT);
 
     commands.spawn(Camera2d);
 
@@ -30,7 +28,7 @@ fn setup(
             let (a, b) = compute_grid_coordinate(x, y);
             commands.spawn((
                 Mesh2d(meshes.add(Rectangle::new(UNIT, UNIT))),
-                MeshMaterial2d(materials.add(hex_color(EMPTY_COLOR))),
+                MeshMaterial2d(materials.add(TileType::No.get_color())),
                 Transform::from_xyz(a, b, 0.0),
                 TileComponent { x, y },
             ));
@@ -38,8 +36,10 @@ fn setup(
     }
 }
 
+/// This is a helper function that can convert a hex encoded i32 to a bevy Color
 fn hex_color(hex: i32) -> Color {
     let mut hex = hex;
+
     let blue = (hex % 256) as f32 / 256.0;
     hex = hex / 256;
     let green = (hex % 256) as f32 / 256.0;
@@ -49,13 +49,16 @@ fn hex_color(hex: i32) -> Color {
     Color::srgb(red, green, blue)
 }
 
+/// This is a helper function that helps to compute the position of the grid tiles to the transform on screen
 fn compute_grid_coordinate(x: usize, y: usize) -> (f32, f32) {
     (
-        UNIT / 2.0 + UNIT * x as f32 - UNIT * 10.0 / 2.0,
-        UNIT / 2.0 - UNIT * y as f32 + UNIT * 20.0 / 2.0 - UNIT,
+        UNIT / 2.0 + UNIT * x as f32 - UNIT * WIDTH / 2.0,
+        UNIT / 2.0 - UNIT * y as f32 + UNIT * HEIGHT / 2.0 - UNIT,
     )
 }
 
+/// Enum for all the different types a tile could be
+/// This basically maps to color
 #[derive(Copy, Clone)]
 enum TileType {
     I,
@@ -68,6 +71,22 @@ enum TileType {
     No,
 }
 
+impl TileType {
+    fn get_color(&self) -> Color {
+        match self {
+            TileType::I => hex_color(0x00ffff),
+            TileType::J => hex_color(0x0000ff),
+            TileType::L => hex_color(0xff7f00),
+            TileType::O => hex_color(0xffff00),
+            TileType::S => hex_color(0x00ff00),
+            TileType::Z => hex_color(0xff0000),
+            TileType::T => hex_color(0x800080),
+            TileType::No => hex_color(0x2b2b2b),
+        }
+    }
+}
+
+/// The World keeps track of what type which tiles are.
 #[derive(Resource)]
 struct World {
     grid: [[TileType; 10]; 20],
@@ -81,6 +100,7 @@ impl World {
     }
 }
 
+/// The TileComponent is a bevy component that sets which tile of the grid it maps to
 #[derive(Component)]
 struct TileComponent {
     x: usize,
@@ -88,23 +108,13 @@ struct TileComponent {
 }
 
 fn tile_update(
-    mut query: Query<(&mut MeshMaterial2d<ColorMaterial>, &TileComponent)>,
+    mut query: Query<(&MeshMaterial2d<ColorMaterial>, &TileComponent)>,
     world: Res<World>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    query.iter_mut().for_each(|(mut mat, tile)| {
+    query.iter_mut().for_each(|(mat, tile)| {
         let tile: TileType = world.grid[tile.y][tile.x];
-        let color = match tile {
-            TileType::I => hex_color(0x00ffff),
-            TileType::J => hex_color(0x0000ff),
-            TileType::L => hex_color(0xff7f00),
-            TileType::O => hex_color(0xffff00),
-            TileType::S => hex_color(0x00ff00),
-            TileType::Z => hex_color(0xff0000),
-            TileType::T => hex_color(0x800080),
-            TileType::No => hex_color(EMPTY_COLOR),
-        };
         let material = materials.get_mut(mat.id()).unwrap();
-        material.color = color;
+        material.color = tile.get_color();
     });
 }
