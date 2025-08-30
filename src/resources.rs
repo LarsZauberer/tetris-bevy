75ca3e2913c4524e65bbfa0a089ad7622b719f83
LarsZauberer <wasser.ian@gmail.com>
2025-08-30 16:53:47 +0200

    World display debug function added

--- src/resources.rs
+++ src/resources.rs
@@ -3,6 +3,7 @@ use crate::{
     utils::{BlockType, CurrentBlock},
 };
 use bevy::prelude::*;
+use std::fmt;
 
 /// The World keeps track of what type which tiles are.
 #[derive(Resource)]
@@ -17,6 +18,18 @@ impl World {
             grid: [[BlockType::No; WIDTH as usize]; HEIGHT as usize],
             current: CurrentBlock::new(),
         }
+    }
+}
+
+impl fmt::Display for World {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in &self.grid {
+            for block in row {
+                write!(f, "{}", block)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
     }
 }
 
